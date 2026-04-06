4b2146de6abd130e83aaae071f5c96a8070291a1
Ankush <jhaankush47@gmail.com>
2026-04-06 11:30:46 +0530

    folder structure is added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "generative ai diagnosis project",
+    version = '0.0.0',
+    author= "Ankush jha",
+    author_email="jhaankush47@gmail.com",
+    packages=find_packages(),
+    install_requires= []
+)
